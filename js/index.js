f182aa8a07ed461963329fe7fca583dae7132e0e
sunshine0427 <903080577@qq.com>
2017-01-04 00:15:28 +0800

    test

--- js/index.js
+++ js/index.js
@@ -6,32 +6,38 @@ $(document).ready(function(){
     });
 });
 
-  function submit1(){  
- 
+  function submit1(){ 
+      // alert(document.getElementById("user1").value); 
       $.ajax({
         type:"POST",
-        url:"login.php",
+        url:"php/login.php",
         datatype:"json",
+        async: false,
         data:{
           name:document.getElementById("user1").value,
           password:document.getElementById("pass1").value,
           type:1
         },
         success: function(data){
+          alert("success");
           var json=eval('('+data+')');
           alert(json);
           alert(json.result);
           alert(json.id);
         },
-      });
-   }  
+        error: function(jqXHR){     
+		   alert("发生错误：" + jqXHR.status);  
+		  },   
+    });
+  }  
 
   function submit2(){  
      // var a=document.getElementById("user2").value;  
      // alert(a);  
      $.ajax({
       type:"POST",
-      url:"login.php",
+      url:"php/login.php",
+      async: false,
       data:{
         name:document.getElementById("user2").value,
         password:document.getElementById("pass2").value,
@@ -44,5 +50,8 @@ $(document).ready(function(){
         alert(json.result);
         alert(json.id);
       },
+      error: function(jqXHR){     
+		   alert("发生错误：" + jqXHR.status);  
+		},   
     });
     } 
