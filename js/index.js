5b9449d754a1b8f1a30410a2cdae46824dd8e451
sunshine0427 <903080577@qq.com>
2017-01-03 23:45:53 +0800

    addmerchant

--- js/index.js
+++ js/index.js
@@ -5,3 +5,44 @@ $(document).ready(function(){
       }
     });
 });
+
+  function submit1(){  
+ 
+      $.ajax({
+        type:"POST",
+        url:"login.php",
+        datatype:"json",
+        data:{
+          name:document.getElementById("user1").value,
+          password:document.getElementById("pass1").value,
+          type:1
+        },
+        success: function(data){
+          var json=eval('('+data+')');
+          alert(json);
+          alert(json.result);
+          alert(json.id);
+        },
+      });
+   }  
+
+  function submit2(){  
+     // var a=document.getElementById("user2").value;  
+     // alert(a);  
+     $.ajax({
+      type:"POST",
+      url:"login.php",
+      data:{
+        name:document.getElementById("user2").value,
+        password:document.getElementById("pass2").value,
+        type:2
+      },
+      datatype:"json",
+      success: function(data){
+        var json=eval('('+data+')');
+        alert(json);
+        alert(json.result);
+        alert(json.id);
+      },
+    });
+    } 
