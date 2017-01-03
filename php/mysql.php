e4d4b4fd588a68fd4c6569b85f1a020ecc052913
luxiaodou <lusongchn@gmail.com>
2017-01-03 23:32:07 +0800

    添加json 修改login

--- php/mysql.php
+++ php/mysql.php
@@ -10,9 +10,9 @@ define('MYSQL_HOST','localhost');
 define('MYSQL_ROOT','root');
 define('MYSQL_ROOT_PW','');
 define('MYSQL_USER','user');
-define('MYSQL_USER_PW','user');
+define('MYSQL_USER_PW','');
 define('MYSQL_SHOP','shop');
-define('MYSQL_SHOP_PW','shop');
+define('MYSQL_SHOP_PW','');
 /**
  * 管理员连接数据库所用函数
  * @return resource
@@ -21,28 +21,35 @@ function rconnectDb() {
     $conn = mysql_connect(MYSQL_HOST,MYSQL_ROOT,MYSQL_ROOT_PW);
     mysql_query("set names 'utf8'");    //解决中文乱码
     if (!$conn){
-        die('Cannot access to DB');
+        die('Cannot access to DB,root');
     }
     mysql_select_db('db_work');
     return $conn;
 }
 
+/**
+ * 用户连接数据库所用的函数
+ * @return resource
+ */
 function uconnectDb() {
     $conn = mysql_connect(MYSQL_HOST,MYSQL_USER,MYSQL_USER_PW);
     mysql_query("set names 'utf8'");    //解决中文乱码
     if (!$conn){
-        die('Cannot access to DB');
+        die('Cannot access to DB,user');
     }
     mysql_select_db('db_work');
     return $conn;
 }
 
-
+/**
+ * 商家连接数据库所用的函数
+ * @return resource
+ */
 function sconnectDb() {
     $conn = mysql_connect(MYSQL_HOST,MYSQL_SHOP,MYSQL_SHOP_PW);
     mysql_query("set names 'utf8'");    //解决中文乱码
     if (!$conn){
-        die('Cannot access to DB');
+        die('Cannot access to DB,shop');
     }
     mysql_select_db('db_work');
     return $conn;
